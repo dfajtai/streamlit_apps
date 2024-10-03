c78d88ae3f9ddfdb7b1a80cfd655ca61a48d878a
Daniel Fajtai <daniel.fajtai@gmail.com>
2024-10-03 15:47:19 +0200

    Update fof2_app.py

--- FOF2/fof2_app.py
+++ FOF2/fof2_app.py
@@ -48,7 +48,7 @@ specimens = specimen_sorting(df['specimen'].unique())
 target_specimens = specimen_sorting(['FOF2-1', 'FOF2-13', 'FOF2-25', 'FOF2-2', 'FOF2-9', 'FOF2-11', 'FOF2-28', 'FOF2-17', 'FOF2-20'])
 
 
-if not len(specimens):
+if len(specimens)==0:
     st.warning("Select at least one specimen")
 
 selected_specimens = st.multiselect(
@@ -57,76 +57,82 @@ selected_specimens = st.multiselect(
     target_specimens
 )
 
+if len(specimens)>0:
+    with st.expander("Measurements of the selected subjects"):
+        st.table(df[df['specimen'].isin(selected_specimens)][['specimen','start','end','duration[s]','duration']])
 
-block_dt = st.slider(
-    "Maximim distance in MINUTES between measurements",
-    min_value=0,max_value=720,value=90, step=5
-)
 
-def get_random_color():
-    """Generate a random hex color."""
-    return f"#{random.randint(0, 0xFFFFFF):06x}"
+    block_dt = st.slider(
+        "Maximim distance in MINUTES between measurements",
+        min_value=0,max_value=720,value=90, step=5
+    )
 
-for s in specimen_sorting(selected_specimens):
-    # Filter data for the selected specimen
-    filtered_df = df[df.specimen == s].copy().reset_index(drop=True)
-    if len(filtered_df.index)==0:
-        continue
-    
-    with st.container(border=True):
-        st.header(s, divider=False)
+    def get_random_color():
+        """Generate a random hex color."""
+        return f"#{random.randint(0, 0xFFFFFF):06x}"
+
+
+
+    for s in specimen_sorting(selected_specimens):
+        # Filter data for the selected specimen
+        filtered_df = df[df.specimen == s].copy().reset_index(drop=True)
+        if len(filtered_df.index)==0:
+            continue
         
-        
-        # Initialize block column
-        filtered_df['block'] = 1
-        
-        block = 1
-        # Loop through the rows and calculate the time difference (dt)
-        for index in range(1, len(filtered_df)):
-            prev_end = filtered_df.iloc[index - 1]['end']
-            current_start = filtered_df.iloc[index]['start']
+        with st.container(border=True):
+            st.header(s, divider=False)
             
-            # Calculate time difference in hours between the previous row's 'end' and current row's 'start'
-            dt = (current_start - prev_end) / pd.Timedelta(minutes=1)  # Calculate the difference in hours
             
-            # Check if the time difference is more than X minutes
-            if dt > block_dt:
-                block += 1
+            # Initialize block column
+            filtered_df['block'] = 1
             
-            filtered_df.at[index, 'block'] = block
+            block = 1
+            # Loop through the rows and calculate the time difference (dt)
+            for index in range(1, len(filtered_df)):
+                prev_end = filtered_df.iloc[index - 1]['end']
+                current_start = filtered_df.iloc[index]['start']
+                
+                # Calculate time difference in hours between the previous row's 'end' and current row's 'start'
+                dt = (current_start - prev_end) / pd.Timedelta(minutes=1)  # Calculate the difference in hours
+                
+                # Check if the time difference is more than X minutes
+                if dt > block_dt:
+                    block += 1
+                
+                filtered_df.at[index, 'block'] = block
 
-        filtered_df['color'] = filtered_df['block'].apply(lambda x: get_random_color())
+            filtered_df['color'] = filtered_df['block'].apply(lambda x: get_random_color())
 
-        # Plotting each block
-        for b in range(1,block+1):
-            sub_df = (filtered_df[filtered_df["block"] == b]).copy().sort_values(by="start").reset_index(drop=True)
-            
-            st.subheader(f"[Block {b}]", divider=True)
-            st.write(f"Number of measurements: {len(sub_df.index)} of {len(filtered_df.index)}")
-            # Set min and max time ranges for the block
-            min_dt = sub_df['start'].min()
-            max_dt = sub_df['end'].max()
-            
-            # Plot the timeline using Plotly Express
-            fig = px.timeline(
-                sub_df,
-                x_start='start',
-                x_end='end',
-                y='specimen',
-                color = 'color',
-                hover_data = ['start','end','duration[s]'],
-                height = 150,
-                # range_x=[min_dt, max_dt]
-            )
+            # Plotting each block
+            for b in range(1,block+1):
+                sub_df = (filtered_df[filtered_df["block"] == b]).copy().sort_values(by="start").reset_index(drop=True)
+                
+                st.subheader(f"[Block {b}]", divider=True)
+                st.write(f"Number of measurements: {len(sub_df.index)} of {len(filtered_df.index)}")
+                # Set min and max time ranges for the block
+                min_dt = sub_df['start'].min()
+                max_dt = sub_df['end'].max()
+                
+                # Plot the timeline using Plotly Express
+                fig = px.timeline(
+                    sub_df,
+                    x_start='start',
+                    x_end='end',
+                    y='specimen',
+                    color = 'color',
+                    hover_data = ['start','end','duration[s]'],
+                    height = 150,
+                    # range_x=[min_dt, max_dt]
+                )
 
-            # Update layout for better presentation
-            fig.update_yaxes(categoryorder="total ascending")
-            fig.update_layout(showlegend=False)
+                # Update layout for better presentation
+                fig.update_yaxes(categoryorder="total ascending")
+                fig.update_layout(showlegend=False)
 
-            # Display the plot in Streamlit
-            st.plotly_chart(fig)
+                # Display the plot in Streamlit
+                st.plotly_chart(fig)
+                
+                with st.expander("Table"):
+                    st.table(sub_df[['specimen','start','end','duration[s]','duration']])
             
-            with st.expander("Table"):
-                st.table(sub_df[['specimen','start','end','duration[s]','duration']])
-        
-        st.write("")
+            st.write("")
