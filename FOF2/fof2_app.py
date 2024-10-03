73accc189827aec5bb22563c3ea5859f3c5713ff
Daniel Fajtai <daniel.fajtai@gmail.com>
2024-10-03 13:26:49 +0200

    Update fof2_app.py

--- FOF2/fof2_app.py
+++ FOF2/fof2_app.py
@@ -62,7 +62,7 @@ for s in sorted(selected_specimens):
         if index == 0:
             continue
         
-        dt = (row.get('start') - filtered_df.iat[index-1,'end']).astype('timedelta64[h]')
+        dt = (row.get('start') - filtered_df.iloc[index-1]['end']).astype('timedelta64[h]')
         if dt > 24:
             block+=1
         
